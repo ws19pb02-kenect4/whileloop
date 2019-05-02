e164190594df293562faf5e3d4689ce726ada95f
kenect4 <49970431+kenect4@users.noreply.github.com>
2019-05-02 16:25:18 -0400

    Create 30count.py

---
+++ 30count.py
@@ -0,0 +1,15 @@
+"""
+while loop countdown
+
+"""
+
+count = 30 #the final countdown
+
+while count > -1:
+    if count == 0:
+        print("Blast off!")
+
+    else:
+        print(count)
+
+    count = count - 1
